a4f1d81ea171fa7c335d82acfaa56c1d7c2b27d9
Anina Angelova <anina_1999@abv.bg>
2025-07-24 15:11:10 +0300

    condenseArrayToNumber.js

---
+++ Arrays/condenseArrayToNumber.js
@@ -0,0 +1,32 @@
+function condenseArrayToNumber(numbers) {
+
+    if (numbers.length === 1) {
+        console.log(numbers[0]);
+        return;
+    }
+
+    let result = [];
+
+    while (true) {
+
+        for (let i = 0; i < numbers.length - 1; i++) {
+            let firstValue = numbers[i];
+            let secondValue = numbers[i + 1];
+
+            let sum = firstValue + secondValue;
+
+            result.push(sum);
+        }
+
+        if(result.length === 1) {
+            break;
+        }
+        numbers = result;
+        result = [];
+    }
+    console.log(result[0]);
+}
+
+//condenseArrayToNumber([2,10,3]);
+//condenseArrayToNumber([5,0,4,1,2]);
+//condenseArrayToNumber([1]);
