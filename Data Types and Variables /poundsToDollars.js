1df85ecea779f50a6ee32b6244c12be54ef57d9d
Anina Angelova <anina_1999@abv.bg>
2025-07-24 15:04:50 +0300

    poundsToDollars.js

---
+++ Data Types and Variables /poundsToDollars.js
@@ -0,0 +1,9 @@
+function poundsToDollars(num) {
+    let britishPoundToDollar = 1.31;
+    let result = num * britishPoundToDollar;
+
+    console.log(result.toFixed(3));
+}
+
+//poundsToDollars(80);
+//poundsToDollars(39);
