8c6f95e0776e61769ea480d079ee31712e24acd1
Anina Angelova <anina_1999@abv.bg>
2025-07-24 16:35:43 +0300

    cats.js

---
+++ Objects-and-Classes /cats.js
@@ -0,0 +1,24 @@
+function cats(catDataArr) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        meow() {
+            return `${this.name}, age ${this.age} says Meow` 
+        }
+    }
+    for (let catData of catDataArr) {
+        let parsedData = catData.split(' ');
+
+        let catName = parsedData[0];
+        let catAge = parsedData[1];
+
+        let cat = new Cat(catName, catAge);
+
+        console.log(cat.meow());
+    }
+}
+
+//cats(['Candy 1', 'Poppy 3', 'Nyx 2']);
