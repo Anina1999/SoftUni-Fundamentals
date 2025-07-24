a200ad4944b407f746aa779d56a7fb189e47427a
Anina Angelova <anina_1999@abv.bg>
2025-07-24 16:20:31 +0300

    binaryToDecimal.js

---
+++ Data Types and Variables /binaryToDecimal.js
@@ -0,0 +1,9 @@
+function binaryToDecimal(binaryNumber) {
+    let binary = Number(binaryNumber);
+    let decimal = parseInt(binary, 2);
+
+    console.log(decimal);
+}
+
+//binaryToDecimal('00001001');
+//binaryToDecimal('11110000');
