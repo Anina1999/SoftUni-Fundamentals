8bbd47da488b3777e1dfcb92cd486f134ce7531b
Anina Angelova <anina_1999@abv.bg>
2025-07-24 15:08:05 +0300

    sumFirstAndLastArrayElements.js

---
+++ Arrays/sumFirstAndLastArrayElements.js
@@ -0,0 +1,12 @@
+function sumFirstAndLastArrayElements (array) {
+    let first = array[0];
+    let last = array[array.length - 1];
+
+    let result = first + last;
+
+    console.log(result);
+}
+
+//sumFirstAndLastArrayElements([20, 30, 40]);
+//sumFirstAndLastArrayElements([10, 17, 22, 33]);
+//sumFirstAndLastArrayElements([11, 58, 69]);
