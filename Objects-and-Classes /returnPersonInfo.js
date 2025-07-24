03c40fe973c82d8ed1d13bb3de41bf30845a6da8
Anina Angelova <anina_1999@abv.bg>
2025-07-24 16:22:31 +0300

    returnPersonInfo.js

---
+++ Objects-and-Classes /returnPersonInfo.js
@@ -0,0 +1,14 @@
+function returnPersonInfo(firstName, lastName, age) {
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    };
+    return person;
+}
+
+returnPersonInfo("Peter",
+
+"Pan",
+
+"20");
