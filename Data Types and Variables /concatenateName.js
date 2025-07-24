1fef1d307f7314bad705d47dcca24631549c116e
Anina Angelova <anina_1999@abv.bg>
2025-07-24 14:59:31 +0300

    concatenateName.js

---
+++ Data Types and Variables /concatenateName.js
@@ -0,0 +1,9 @@
+function concatenateName(firstName, secondName, delimiter) {
+
+    //let result = firstName + delimiter + secondName;
+    
+    console.log(`${firstName}${delimiter}${secondName}`);
+
+}
+
+//concatenateName('John', 'Smith', '->');
