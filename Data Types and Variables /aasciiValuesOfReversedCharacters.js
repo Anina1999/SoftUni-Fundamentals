c9cc7c91ab4bd35b52e3af780230e9babc44f81e
Anina Angelova <anina_1999@abv.bg>
2025-07-24 15:03:45 +0300

    aasciiValuesOfReversedCharacters.js

---
+++ Data Types and Variables /aasciiValuesOfReversedCharacters.js
@@ -0,0 +1,6 @@
+function aasciiValuesOfReversedCharacters(param1, param2, param3) {
+    console.log(`${param3}${param2}${param1}`);
+    console.log(`${param3.charCodeAt(0)} ${param2.charCodeAt(0)} ${param1.charCodeAt(0)}`);
+}
+
+//aasciiValuesOfReversedCharacters('a', 'b', 'c');
