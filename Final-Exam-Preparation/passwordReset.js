087d1d0f10bdfda9bef1f1894bc37b8cc2887ff6
Anina Angelova <anina_1999@abv.bg>
2025-07-25 22:50:39 +0300

    Update passwordReset.js

--- Final-Exam-Preparation/passwordReset.js
+++ Final-Exam-Preparation/passwordReset.js
@@ -1,3 +1,4 @@
+//solve #1
 function passwordReset(inputArr) {
     let password = inputArr.shift();
 
@@ -49,3 +50,46 @@ function passwordReset(inputArr) {
 //"Substitute :: -",
 //"Substitute | ^",
 //"Done"]);
+
+
+//solve #2
+function passwordReset(input) {
+    let password = input.shift();
+    for (let commandLine of input) {
+        if (commandLine === 'Done') {
+            console.log(`Your password is: ${password}`);
+            break;
+        }
+        let [command, ...args] = commandLine.split(' ');
+
+        switch (command) {
+            case 'TakeOdd':
+                let newPassword = '';
+                for (let i = 1; i < password.length; i += 2) {
+                    newPassword += password[i];
+                }
+                password = newPassword;
+                console.log(password);
+                break;
+            
+            case 'Cut':
+                let index = Number(args[0]);
+                let length = Number(args[1]);
+                let substring = password.substring(index, index + length);
+                password = password.replace(substring, '');
+                console.log(password);
+                break;
+
+            case 'Substitute':
+                let substr = args[0];
+                let replacement = args[1];
+                if (password.includes(substr)) {
+                    let regex = new RegExp(substr, "g"); //търси всички съвпадения, а не само първото
+                    password = password.replace(regex, replacement);
+                    console.log(password);
+                } else {
+                    console.log('Nothing to replace!');
+                }
+        }
+    }
+}
